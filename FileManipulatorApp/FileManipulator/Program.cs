3abc7326d3339d0491b412c6c5ebc52294c5ffc2
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2022-01-12 10:51:37 +0100

    Created a basic GUI.

--- FileManipulatorApp/FileManipulator/Program.cs
+++ FileManipulatorApp/FileManipulator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace FileManipulator
 {
@@ -6,7 +9,29 @@ namespace FileManipulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            switch (args[0].ToLower())
+            {
+                case "create":
+                    Console.WriteLine("Choosen action create");
+                    break;
+                case "rename":
+                    Console.WriteLine("Choosen action rename");
+                    break;
+                case "delete":
+                    Console.WriteLine("Choosen action delete");
+                    break;
+                case "replace":
+                    Console.WriteLine("Choosen action replace");
+                    break;
+                case "help":
+                    Console.WriteLine("Help needs to be implemented.");
+                    break;
+                default:
+                    Console.WriteLine("Action was not recognized, use help for more information.");
+                    break;
+            }
+
         }
     }
+
 }
